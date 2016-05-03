6d9fb8cf584129ac9b212f9c9881c1019a8fcd2c
Andreas Willich <andreas.willich@techtalk.at>
2016-05-03 15:19:51 +0200

    NUnit3 added

---
+++ NUnit3/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace NUnit3
+{
+    [Binding]
+    public class Class1
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+
+        }
+
+    }
+}
