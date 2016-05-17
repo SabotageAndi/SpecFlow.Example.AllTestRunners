a52623286f9d8352008285bebfe5a27064fc5eca
Andreas Willich <andreas.willich@techtalk.at>
2016-05-17 16:56:40 +0200

    add MSTest & SpecFlow Upgrade

---
+++ MSTest/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace MSTest
+{
+    [Binding]
+    public class Class1
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+        }
+
+    }
+}
