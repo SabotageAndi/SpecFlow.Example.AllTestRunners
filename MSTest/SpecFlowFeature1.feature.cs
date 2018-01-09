b1f3b1e90cfac4626c21e1dcef0569a8223024d7
Andreas Willich <andreas.willich@techtalk.at>
2018-01-09 11:43:34 +0100

    update all packages

--- MSTest/SpecFlowFeature1.feature.cs
+++ MSTest/SpecFlowFeature1.feature.cs
@@ -23,8 +23,22 @@ namespace MSTest
         
         private static TechTalk.SpecFlow.ITestRunner testRunner;
         
+        private Microsoft.VisualStudio.TestTools.UnitTesting.TestContext _testContext;
+        
 #line 1 "SpecFlowFeature1.feature"
 #line hidden
+        
+        public virtual Microsoft.VisualStudio.TestTools.UnitTesting.TestContext TestContext
+        {
+            get
+            {
+                return this._testContext;
+            }
+            set
+            {
+                this._testContext = value;
+            }
+        }
         
         [Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute()]
         public static void FeatureSetup(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext testContext)
@@ -61,6 +75,7 @@ namespace MSTest
         public virtual void ScenarioSetup(TechTalk.SpecFlow.ScenarioInfo scenarioInfo)
         {
             testRunner.OnScenarioStart(scenarioInfo);
+            testRunner.ScenarioContext.ScenarioContainer.RegisterInstanceAs<Microsoft.VisualStudio.TestTools.UnitTesting.TestContext>(TestContext);
         }
         
         public virtual void ScenarioCleanup()
